aa92c97c127677aedf1c8d45ad276aec109c0ca3
InnovativeSumit <pal2024santosh@gmail.com>
2025-04-16 13:06:12 +0530

    ARITHMATIC-OPERATORS
    Arithmetic operators perform basic math operations:
    + (Addition)
    - (Subtraction)
    * (Multiplication)
    / (Division)
    % (Modulus: remainder)

---
+++ OPERATORS/ARITHMETIC_OPERATORS.c
@@ -0,0 +1,25 @@
+//make by sumit
+#include<stdio.h>
+int main(){
+    int a,b,c,d,e,f,g;
+a=20;
+b=2;
+printf("THE ADDITION IS       : %d\n",(a+b));
+printf("THE SUBTRACTION IS    : %d\n",(a-b));
+printf("THE MULTIPLICATION IS : %d\n",(a*b));
+printf("THE DIVISION IS       : %d\n",(a/b));
+printf("THE REMIANDER IS      : %d\n\n\n",(a%b));  //%-> MODULO OPERATORS
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+g=a%b;
+printf("NOW WE STORE THE OPERSTION IN OTHER VARIABLE\n");
+printf("============================================\n");
+printf("THE ADDITION IS       : %d\n",c);
+printf("THE SUBTRACTION IS    : %d\n",d);
+printf("THE MULTIPLICATION IS : %d\n",e);
+printf("THE DIVISION IS       : %d\n",f);
+return 0;
+}
+
