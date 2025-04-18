28dae6d6a8f1abbcab78a88ddaf09c1ae048a325
InnovativeSumit <pal2024santosh@gmail.com>
2025-04-18 10:16:55 +0530

    RELATIONAL-OPERATORS
    Key Relational Operators
    Equal to (==)
    Checks if two values are the same.
    Example: 5 == 5 → True
    
    Not equal to (!= or <>)
    Checks if two values are different.
    Example: 5 != 3 → True
    
    Greater than (>)
    Checks if the left value is larger than the right.
    Example: 10 > 7 → True
    
    Less than (<)
    Checks if the left value is smaller than the right.
    Example: 4 < 6 → True
    
    Greater than or equal to (>=)
    Checks if the left value is larger or equal to the right.
    Example: 8 >= 8 → True
    
    Less than or equal to (<=)
    Checks if the left value is smaller or equal to the right.
    Example: 5 <= 5 → True

---
+++ OPERATORS/RELATIONAL_OPERATORS/RELATIONAL_OPERATORS.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+int a = 10, b = 20;
+// Equal to (==)
+printf("%d == %d is %d\n",a,b,a==b);  // 0 (false)
+
+// Not equal to (!=)
+printf("%d != %d is %d\n",a,b,a!= b);  // 1 (true)
+
+// Greater than (>)
+printf("%d > %d is %d\n",a,b,a>b);    // 0 (false)
+
+// Less than (<)
+printf("%d < %d is %d\n",a,b,a<b);    // 1 (true)
+
+// Greater than or equal to (>=)
+printf("%d >= %d is %d\n",a,b,a>= b);  // 0 (false)
+
+// Less than or equal to (<=)
+printf("%d <= %d is %d\n", a, b,a<= b);  // 1 (true)
+
+return 0;
+}
