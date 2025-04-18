66cdcf45cd7c0379d17ec445d117103c91baf4af
InnovativeSumit <pal2024santosh@gmail.com>
2025-04-18 10:37:49 +0530

    EQUALITY-OPERATORS
    Equal to (==)
    Checks if two values are the same.
    Example: 5 == 5 → True
    
    Not equal to (!= or <>)
    Checks if two values are different.
    Example: 5 != 3 → True

---
+++ OPERATORS/EQUALITY-OPERATORS.c
@@ -0,0 +1,14 @@
+// MAKE BY SUMIT
+#include<stdio.h>
+int main() {
+int a = 3;
+int b = 4;
+
+// == represents equals to
+printf("%d\n", a == b); // Output: 0 (false)
+
+// != represents not equals to
+printf("%d\n", a != b); // Output: 1 (true)
+
+return 0;
+}
