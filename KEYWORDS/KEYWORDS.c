af1fdc8e0474d5deed60af1babfe86a8ffa664d3
InnovativeSumit <pal2024santosh@gmail.com>
2025-04-21 12:07:16 +0530

    KEYWORDS

---
+++ KEYWORDS/KEYWORDS.c
@@ -0,0 +1,47 @@
+/*C LANGUAGE KEYWORDS (C99 Standard) */
+/*
+
+auto, break, case, char, const, continue, default, do, double, else, enum,
+extern, float, for, goto, if, inline, int, long, register, restrict, return,
+short, signed, sizeof, static, struct, switch, typedef, union, unsigned,
+void, volatile, while, _Alignas, _Alignof, _Atomic, _Bool, _Complex,
+_Generic, _Imaginary, _Noreturn, _Static_assert, _Thread_local
+*/
+
+/*OMMON C STANDARD LIBRARY FUNCTIONS */
+
+/* <stdio.h> */
+/*
+printf(), scanf(), getchar(), putchar(), gets(), puts(),
+fopen(), fclose(), fread(), fwrite(), fprintf(), fscanf(),
+fseek(), ftell(), rewind(), fflush()
+*/
+
+/* <stdlib.h> */
+/*
+malloc(), calloc(), realloc(), free(), atoi(), atof(), atol(),
+abs(), system(), exit(), rand(), srand(), qsort(), bsearch()
+*/
+
+/* <string.h> */
+/*
+strlen(), strcpy(), strncpy(), strcat(), strncat(), strcmp(), strncmp(),
+strchr(), strrchr(), strstr(), memset(), memcpy(), memmove()
+*/
+
+/* <ctype.h> */
+/*
+isalnum(), isalpha(), isdigit(), islower(), isupper(), isspace(),
+tolower(), toupper()
+*/
+
+/* <math.h> */
+/*
+sqrt(), pow(), ceil(), floor(), fabs(), sin(), cos(), tan(), log(), exp()
+*/
+
+/*NOTE:
+   - Keywords are reserved; don’t use them as identifiers.
+   - Many more functions exist in standard libraries, these are just the most used.
+   - C11 introduced more keywords (e.g., _Alignas), but C99 covers most practical needs.
+*/
