ee5359041fd554f52c4232ff01665460ee4617a8
InnovativeSumit <pal2024santosh@gmail.com>
2025-04-27 09:02:21 +0530

    TERNARY-OPERATOR
    The ternary operator is a shorthand way of writing an if-else statement.
    It’s called "ternary" because it involves three operands.
    
    condition → an expression that is either true or false
    expression_if_true → runs if the condition is true
    expression_if_false → runs if the condition is false

---
+++ OPERATORS/TERNARY_OPERATOR.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+int a = 10, b = 20,max;
+   // Using ternary operator to find the maximum
+max = (a > b) ? a : b;
+printf("The maximum value is: %d\n", max);
+return 0;
+}
+
